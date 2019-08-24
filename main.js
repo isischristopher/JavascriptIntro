c5181bbaaaed49aeec7464d8fac7718d1391ed49
Isis Christopher <isischristopher@gmail.com>
2019-08-24 13:43:09 -0500

    setup

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hi');
+console.log('i lava  you');
+
+const pintToDOM= (toPrint) => {
+    document.getElementById('output').innerHTML = toPrint;
+}
+
+let bandNumber = 1
+
+const takeNumber =  (bandName) => {
+  /*
+    Write your awesome code here. See comments
+    below for what should be returned.
+  */
+printToDOM('<h3>${bandNumber}) ${bandName})</h3>');
+ bandNumber++
+  // you should already have this function written from the class lecture
+};
+
+takeNumber("Galactic Scum") // This should display "1. Galactic Scum" in the DOM
+takeNumber("Underdogs") // This should display "2. Underdogs" in the DOM
+
+
+pintToDOM('Isis')
